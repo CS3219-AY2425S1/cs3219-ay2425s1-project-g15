faa97351d0d83e8d07b4802800a15cec672d6c05
ChryslineLim <chryslinelim@gmail.com>
2024-11-12 17:44:16 +0800

    enhance collab page UI

--- frontend/src/app/collaboration/components/question.tsx
+++ frontend/src/app/collaboration/components/question.tsx
@@ -229,8 +229,8 @@ const Question = ({
   );
 
   return (
-    <div className="px-12 grid grid-rows-[20%_45%_35%] gap-4 grid-cols-1 h-full items-start">
-      <div className="mt-10 row-span-1 grid grid-rows-1 grid-cols-[75%_25%] w-full">
+    <div className="px-2 grid grid-rows-[20%_45%_35%] gap-2 grid-cols-1 h-full items-start">
+      <div className="mt-4 row-span-1 grid grid-rows-1 grid-cols-[75%_25%] w-full h-full">
         <div className="flex flex-col" ref={containerRef}>
           <h1
             className="text-yellow-500 text-xl font-bold pb-2 truncate"
@@ -284,11 +284,11 @@ const Question = ({
           Exit Room
         </Button>
       </div>
-      <span className="row-span-1 text-primary-300 text-md max-h-[100%] h-full overflow-y-auto flex flex-col gap-2 bg-primary-800 p-3 rounded-md">
-        <span className="text-yellow-500 font-bold">Question Description</span>
-        <span className="text-white py-8 text-md">{question?.description}</span>
+      <span className="row-span-1 text-primary-300 max-h-[100%] h-full overflow-y-auto flex flex-col bg-primary-800 p-3 rounded-md">
+        <span className="text-yellow-500 font-bold text-md">Question Description</span>
+        <span className="text-white py-2 text-xs">{question?.description}</span>
       </span>
-      <div className="row-span-1 flex flex-col bg-primary-800 rounded-md h-full max-h-[80%] min-h-[80%] overflow-y-auto">
+      <div className="row-span-1 flex flex-col bg-primary-800 rounded-md h-full max-h-[90%] min-h-[80%] overflow-y-auto">
         {messages.length == 0 ? (
           <span className="h-full w-full flex items-center justify-center text-primary-300 italic">
             Say hello to your match!
