e664af399a8bf855874560dbd834f71618c1028e
jehou <jehousoh@gmail.com>
2024-11-12 15:52:55 +0800

    Add TURN servers

--- frontend/src/app/collaboration/components/video.tsx
+++ frontend/src/app/collaboration/components/video.tsx
@@ -30,7 +30,19 @@ const VideoCall = ({ provider }: VideoCallProps) => {
 
   useEffect(() => {
     const pc = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        {
+          urls: "turn:global.relay.metered.ca:80",
+          username: "2b012e1176eda910d0c8a755",
+          credential: "6KjvgYahJZ21gdAp",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80?transport=tcp",
+          username: "2b012e1176eda910d0c8a755",
+          credential: "6KjvgYahJZ21gdAp",
+        }
+      ],
     });
 
     pc.onicecandidate = (event) => {
